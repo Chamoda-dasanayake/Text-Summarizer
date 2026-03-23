2dc29bb9d7f4f227719bb2ca2ce0fd82af6474ae
Chamoda-Dasanayake <dasanay-ec21074@stu.kln.ac.lk>
2026-03-23 23:34:05 +0530

    add data transformation

--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -7,3 +7,9 @@ class DataIngestionConfig:
     dataset_name: str  # Added this to hold the HF ID
     local_data_file: Path
     unzip_dir: Path
+
+@dataclass
+class DataTransformationConfig:
+    root_dir: Path
+    data_path: Path
+    tokenizer_name: Path
